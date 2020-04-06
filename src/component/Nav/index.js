89b525fa533f60d0d2f81aaf97bf74c8c212ef76
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-04-05 22:06:14 -0700

    added index file and pix

---
+++ src/component/Nav/index.js
@@ -0,0 +1,17 @@
+import React from React;
+
+function Header(){
+  return (
+    <div>
+    <nav>
+    <div className="nav-wrapper">
+    <div className="title center">
+    Employee Directory
+    </div>
+    </div>
+    </nav>
+    </div>
+  );
+}
+
+export default Header;
