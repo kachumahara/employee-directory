c9725b84fdfc5b1e2e47437579b3d82e097d2979
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-04-05 21:05:52 -0700

    created the searchForm js

---
+++ src/component/searchForm.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function SearchForm(props) {
+  return (
+    <div>
+    <input
+    onChange={props.handleInputChange}
+    value={props.value} 
+    name="search"
+    type="text"
+    className="form-control"
+    placeholder="Search"
+    id="search"/>
+    </div>
+  );
+}
+export default SearchForm;
