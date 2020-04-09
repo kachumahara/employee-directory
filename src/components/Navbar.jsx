88680fcecb301631187be917f581217e6e1edd13
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-04-08 21:47:37 -0700

    added name css header css navbar css and header jsx functions

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import name from "./Name.jsx";
+import "../style/Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+        data-target="#navbarNav"
+      >
+        <span className="navbar-toggler-icon" />>
+      </button>
+      <div className="collapse navbar-collapse row">
+        <div className="search-area col-4">
+          <Name />
+        </div>
+      </div>
+    </nav>
+  )
+
+}
+
+export default Navbar;
