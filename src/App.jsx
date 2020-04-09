f9da527be3d451d4dcc9503c050fd3393ae36801
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-04-08 22:20:55 -0700

    good

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import OmdbContainer from "./components/OmdbContainer";
+import Wrapper from "./components/Wrapper.jsx";
+import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
 
 function App() {
-  return <OmdbContainer />;
+ return (
+  <div className="App">
+  <Wrapper>
+      <Header />
+      <Main />
+  </Wrapper>
+</div>
+ )
 }
 
 export default App;
