3b9cf62e325398c8d8eebb3bb292b4ac6f947e57
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-04-08 20:15:17 -0700

    good

--- src/components/TableArea.jsx
+++ src/components/TableArea.jsx
@@ -1,17 +1,27 @@
+import React, { useState, useEffect } from "react";
+import TableData from "./TableData.jsx";
+import Navbar from "./Navbar.jsx";
+import API from "../utils/API.js";
+import TableContext from "../utils/TableContext";
 
-function SearchForm(props) {
-  return (
-    <div>
-      <input
-        onChange={props.handleInputChange}
-        value={props.value}
-        name="search"
-        type="text"
-        className="form-control"
-        placeholder="Search"
-        id="search"
-      />
-    </div>
-  );
+// useState ----> [devState, setDevState]
+const TableArea = () => {
+  const [devState, setDevState] = useState({
+    users: [],
+    order: 'descend',
+    filteredUsers: [],
+
+    // each heading is comprised of 3 key-value pairs
+    // there are a total of 5 headings
+
+    headings: [
+      {name: 'image', width: '10%', order: 'descend'},
+      {name: 'name', width: '10%', order: 'descend'},
+      {name: 'phone', width: '10%', order: 'descend'},
+      {name: 'email', width: '10%', order: 'descend'},
+      {name: 'dob', width: '10%', order: 'descend'},
+    ]  
+
+  });
+
 }
-export default SearchForm;
