011a3e90971faa3a9f8ccaa6bc26748f05669d85
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-04-08 22:13:08 -0700

    good

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
-import React from React;
+import React from "react";
+import "../styles/Header.css";
 
-function Header(){
+const Header = () => {
   return (
-    <header className="header">
-    <h3>Employee Directory</h3>
-    </header>
+    <div className="header w-100 text-center">
+      <h3>Employee Directory</h3>
+    </div>
   );
-}
+};
 
 export default Header;
---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TableArea from "./TableArea.jsx";
+
+const Main = () => {
+  return (
+    <>
+    <TableArea />
+    </>
+  )
+};
+
+export default Main;
