a88f10683720de8f0cbabd185ce6b6258521256a
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-04-08 21:33:52 -0700

    added the TableArea jsx function

--- src/components/TableArea.jsx
+++ src/components/TableArea.jsx
@@ -62,6 +62,69 @@ const TableArea = () => {
       }
     };
 
-    
+    const usersSorted = devState.filteredUsers.sort(comparer);
+    const headingsUpdated = devState.headings.map(element => {
+      element.order = element.name === heading ? sortOrder : element.order;
+      return element
+
+    });
+    // spread syntax for incorporating consts
+
+    setDevState({
+      ...devState,
+      filteredUsers: usersSorted,
+      headings: headingsUpdated
+    })
+  }
+
+  const deltaHandler = e => {
+    const filter = e.target.value;
+    const listsFiltered = devState.users.filter(item => {
+      let vals = `${item.name.first.toLowerCase()} ${item.name.last.toLowerCase()}`
+      console.log(filter, vals)
+      // indexOf() returns the position of the first occurrence of a substring
+
+      if (vals.indexOf(filter.toLowerCase()) !== -1) {
+        return item
+      }
+    })
+    setDevState({
+      ...devState,
+      filteredUsers: listsFiltered
+    })
   };
+  // useEffect to call loading function w/ react only once
+
+  useEffect(() => {
+    API.getUsers()
+    .then(results => {
+      console.log(results.data.results)
+      setDevState({
+        ...devState,
+        users: results.data.results,
+        filteredUsers: results.data.results
+      })
+    })
+  },
+  // If CB present -->[]--> useEffect will only activate if vals in the list change
+    // that is, if deltaHandler is executed due to search changes 
+  []);
+  // TableContext.Provider used with value (component props)
+    // value contains data made available to component tree
+    // There is another ternary operator here
+    // if devState.filteredUsers.length is greater than 0
+    // then load <TableData /> component
+    // else load an empty div
+
+  return (
+    <TableContext.Provider
+    value={{ devState, deltaHandler, sortHandler}}>
+      <Navbar />
+      <div className="data-area w-90 mx-auto">
+        {devState.filteredUsers.length > 0 ? <TableData /> : <div></div>}
+      </div>
+    </TableContext.Provider>
+  )
 };
+
+export default TableArea;
