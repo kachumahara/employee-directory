3d35ddf479f7d582bd99837501cf88d3a6e4794f
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-04-08 20:43:18 -0700

    good

--- src/components/TableArea.jsx
+++ src/components/TableArea.jsx
@@ -8,20 +8,60 @@ import TableContext from "../utils/TableContext";
 const TableArea = () => {
   const [devState, setDevState] = useState({
     users: [],
-    order: 'descend',
+    order: "descend",
     filteredUsers: [],
 
     // each heading is comprised of 3 key-value pairs
     // there are a total of 5 headings
 
     headings: [
-      {name: 'image', width: '10%', order: 'descend'},
-      {name: 'name', width: '10%', order: 'descend'},
-      {name: 'phone', width: '10%', order: 'descend'},
-      {name: 'email', width: '10%', order: 'descend'},
-      {name: 'dob', width: '10%', order: 'descend'},
-    ]  
-
+      { name: "image", width: "10%", order: "descend" },
+      { name: "name", width: "10%", order: "descend" },
+      { name: "phone", width: "10%", order: "descend" },
+      { name: "email", width: "10%", order: "descend" },
+      { name: "dob", width: "10%", order: "descend" },
+    ],
   });
 
-}
+  const sortHandler = (heading) => {
+    let sortOrder = devState.headings
+      .filter((element) => element.name === heading)
+      .map((element) => element.order)
+      .toString();
+
+    const comparer = (a, b) => {
+      if (sortOrder === "ascend") {
+        if (a[heading] === undefined) {
+          return 1;
+        } else if (b[heading] === undefined) {
+          return -1;
+        } else if (heading === "name") {
+          return a[heading].first.localeCompare(b[heading].first);
+        } else if (heading === "dob") {
+          return a[heading].age - b[heading.age];
+        } else {
+          return a[heading].localeCompare(b[heading]);
+        }
+      }
+
+      // accounting for any missing values...
+      // the order must be strictly equal to descend now (conditional dichotomy)
+      // else, if order==='descend', then, if...(see below)
+      else {
+        if (a[heading] === undefined) {
+          return 1;
+        } else if (b[heading] === undefined) {
+          return -1;
+        } else if (heading === "name") {
+          return b[heading].first.localeCompare(a[heading].first);
+        } else if (heading === "dob") {
+          return b[heading].age - a[heading.age];
+        } else {
+          return b[heading].localeCompare(a[heading]);
+        }
+      }
+    };
+
+    
+  };
+};
